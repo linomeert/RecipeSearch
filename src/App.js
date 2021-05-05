f4b49a200f179da34b60f4f4a183a3420254ef90
Lino Meert <lino.meert@gmail.com>
2021-05-05 17:54:57 +0200

    added event handler with api call to handle change of search field

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 import React from 'react';
 import SearchBar from './components/SearchBar';
 import Recipes from './components/Recipes';
 import Ingredients from './components/Ingredients';
 
 class App extends React.Component {
+  
+  state = {
+    ingredients: {},
+    recipes: {}
+  }
+  
   render() {
     return ( 
-        
-      <div className="RecipeFinder">
+      <div className="recipe-app">
          <div className="ingredients">
             <h2>Ingredients</h2>
-            <SearchBar/>
+            <SearchBar searchIngredients={this.props.searchIngredients}/>
             <Ingredients/>
         </div>
         <div className="recipes">
-            <h2>recipes</h2>
+            <h2>Recipes</h2>
             <Recipes/>
         </div>
       </div>
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,32 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+
+    state = {
+        searchResults: {},
+    };
+
+    constructor(props) {
+        super(props);
+        this.queryRef = React.createRef();
+    }
+
+    searchIngredients = () => {
+        const APIKEY = '540815f48fa1457791cc375205109fa9'
+        const query = this.queryRef.current.value
+        const searchEndpoint = `https://api.spoonacular.com/food/ingredients/autocomplete?apiKey=${APIKEY}&query=${query}&number=5`
+        fetch(searchEndpoint).then(response => response.json()).then((data) => {
+            console.log(data)
+            this.setState(
+                { searchResults: data}
+            )
+        })
+
+    }
+
     render(){
        return (
-           <input type="text" ref={this.queryRef} className="search-ingredient" placeholder="Search ingredient"></input>
+           <input type="text" ref={this.queryRef} onChange={this.searchIngredients} className="search-ingredient" placeholder="Search ingredient"></input>
        )
    } 
 }
