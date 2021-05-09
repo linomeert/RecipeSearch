fe835390348bf86563020f0b27e1800b5912798f
Lino Meert <lino.meert@gmail.com>
2021-05-09 20:11:10 +0200

    still trying to deploy..

--- src/components/Router.js
+++ src/components/Router.js
@@ -5,7 +5,7 @@ import NotFound from "./NotFound";
 import RecipeDetail from "./RecipeDetail";
 
 const Router = () => (
-  <BrowserRouter>
+  <BrowserRouter basename={"/RecipeSearch"}>
     <Switch>
       <Route exact path="/" component={App} />
       <Route exact path="/recipes/:recipeId" component={RecipeDetail} />
